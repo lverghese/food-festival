e69dff7813a63b21e768248bb97aaf66da18157b
lverghese <loganverghese@gmail.com>
2021-10-18 16:56:58 -0400

    more webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = { 
     entry: './assets/js/script.js',
@@ -7,12 +8,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
     }, 
-    plugins:[
-        new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
-        }),
-    ],
+    plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+  })
+],    
     mode: 'development'
 
 };
